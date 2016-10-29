2193189c0ab97c3a04f4beff36a4c6eee3cfecfb
Diego Maroto <diego@debian.domaindebian.com>
2016-10-29 10:04:57 +0200

    Palindrome check

--- problem0004/main.py
+++ problem0004/main.py
@@ -1,12 +1,19 @@
 def isPalindrome(num):
 	phrase = str(num);
-	
-	
+	len = len(phrase);
+	breaklen = len//2;
+	for x in range(0,breaklen):
+		if phrase[x]!=phrase[len-x-1]:
+			return false;
+		if x>=breaklen: break;
+	return true;
 
 def productCombination(digits):
 	min = 10**digits;
 	max = (10**(digits+1))-1;
-
+	
+	
+print(isPalindrome(9009));
 print("Problem 4 of Project Euler: Largest palindrome product.\n");
 print("\tTesting palindrome of two 2-digit numbers:");
 
