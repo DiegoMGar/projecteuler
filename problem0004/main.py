91ab7ad88c2c4a88b9588d17d0108fdb548347da
Diego Maroto <diego@debian.domaindebian.com>
2016-10-29 09:18:31 +0200

    Preparing problem 4 project euler

---
+++ problem0004/main.py
@@ -0,0 +1,17 @@
+def isPalindrome(num):
+	phrase = str(num);
+	
+	
+
+def productCombination(digits):
+	min = 10**digits;
+	max = (10**(digits+1))-1;
+
+print("Problem 4 of Project Euler: Largest palindrome product.\n");
+print("\tTesting palindrome of two 2-digit numbers:");
+
+print("\n");
+print("\tTesting palindrome of two 2-digit numbers:");
+
+print("\n");
+print("\tEND***");
