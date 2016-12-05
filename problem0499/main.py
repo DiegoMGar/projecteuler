099e1b07bdc25816012ba4722171c0af8417ac59
Diego Maroto <diego.diemg@gmail.com>
2016-12-05 20:56:05 +0100

    Starting problem 499

---
+++ problem0499/main.py
@@ -0,0 +1,8 @@
+from time import time;
+print("Euler Project: Problem 499\n");
+print("St. Petersburg Lottery.\n");
+print("Finde p15(109).");
+input("Ready?");
+start = time();
+print("Solution");
+print("Time: "+str(time()-start));
