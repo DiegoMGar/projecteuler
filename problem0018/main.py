6ecfa06e4122d9447f060542c182d7e3a2f1af8f
Diego Maroto <diego.diemg@gmail.com>
2017-11-11 14:04:31 +0100

    Ejercicio a medias, la primera solución implementada es errónea.

--- problem0018/main.py
+++ problem0018/main.py
@@ -1,25 +1,44 @@
 #!/usr/bin/python3.5
 
-pyramid = '''
-75
-95 64
-17 47 82
-18 35 87 10
-20 04 82 47 65
-19 01 23 75 03 34
-88 02 77 73 07 63 67
-99 65 04 28 06 16 70 92
-41 41 26 56 83 40 80 70 33
-41 48 72 33 47 32 37 16 94 29
-53 71 44 65 25 43 91 52 97 51 14
-70 11 33 28 77 73 17 78 39 68 17 57
-91 71 52 38 17 14 91 43 58 50 27 29 48
-63 66 04 68 89 53 67 30 73 16 69 87 40 31
-04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
-'''
-result = 0
+#tupla (fila,numero)
+#obtengo los números más altos de cada fila
+#saco los caminos con más peso que pasen por esos números
+#comparos los resultados y me quedo el mayor
+#me es indiferente qué camino fue, para este problema
 
-#code...
+def path(lista):
+    return false
+def path_reverse(lista):
+    return false
+
+f = open("pyramid.txt")
+pyramid = f.read().splitlines()
+f.close()
+print(pyramid)
+tuplasfuertes = []
+for x in pyramid:
+    nums = x.split(" ")
+    tuplasfuertes.append(max(nums)) #la posición en el arrray es la fila
+print(tuplasfuertes)
+
+
+
+exit()
+
+# el codigo que hay después de este comentario
+# es la primera solución que le dí, errónea
+# pensando que tenía que navegar de mayor en mayor hacia abajo en la pirámide
+# pero tengo que buscar el camino de mayor en toda la pirámide
+
+result = 0
+lastposition=0
+for x in f.read().splitlines():
+    
+    if len(nums)>1:
+        if nums[lastposition]<nums[lastposition+1]:
+            lastposition+=1
+    print(nums[lastposition])
+    result += int(nums[lastposition])
 
 print("Result: ")
 print(int(result))
